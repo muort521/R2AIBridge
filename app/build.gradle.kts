7eb9409d669e31de20f496b0ba71c1e57730b2ee
a422015028 <55303265+a422015028@users.noreply.github.com>
2026-02-04 18:03:04 +0800

    Release v3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.r2aibridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 260202
-        versionName = "3.3"
+        versionCode = 260204
+        versionName = "3.4"
         
         ndkVersion = "25.2.9519653"
 
